6ee12b3e4005c372cbc477a5a8be4c2e80c1663a
Noluthando September <NSeptember591@gmail.com>
2025-11-21 15:39:35 +0200

    updated

--- todo_web/todo/views.py
+++ todo_web/todo/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def landing_page(request):
+    if request.method == 'POST':
+        
+        selected = []
+        if "personal" in request.POST:
+            selected.append("personal")
+        if "school" in request.POST:
+            selected.append("school")
+        if "work" in request.POST:
+            selected.append("work")
+        
+        if not selected:
+            error_message = "Please select at least one category!"
+            return render(request, 'todo/landing.html', {'error_message': error_message})
+        
+        request.session["selected_categories"] = selected
+        
+        return redirect("/create-todo/")
+    
     return render(request, 'todo/landing.html')
-# Create your views here.
